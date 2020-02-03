8f3942e60a8ae22908a753d681fc0894c3e46baa
daan <2087884@talnet.nl>
2020-02-03 13:44:26 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo ("ik ga twee getallen vermedigvuldigen")."\n";
+    echo ("eerste getal?");
+    $een = readline($prompt);
+    echo ("tweede getal?");
+    $twee = readline($prompt);
+    echo ($een * $twee)
+
+?>
